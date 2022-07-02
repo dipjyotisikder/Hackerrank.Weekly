a60ee724a236b0f877d534fec26f6f21cb916b66
dipjyoti <dip@mailinator.com>
2022-07-03 04:20:52 +0600

    Added Odd Numbers problem and solution.

---
+++ Hackerrank.Day1/Hackerrank.Day1.OddNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿//This solution will find all the Odd numbers between two given limit.
+
+class Result
+{
+    public static List<int> findNumber(int l, int r)
+    {
+        var totalDistance = l - r;
+        var odds = new List<int>();
+
+        for (int i = 0, value = l; i <= totalDistance; i++, value++)
+        {
+            if (value % 10 != 0) odds.Add(i);
+        }
+
+        return odds;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Result.findNumber(4, 10);
+    }
+}
