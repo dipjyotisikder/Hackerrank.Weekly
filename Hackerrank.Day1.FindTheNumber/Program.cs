1403769a9f8a1434d1640692cac6c073788b92aa
dipjyoti <dip@mailinator.com>
2022-07-02 18:11:04 +0600

    Added a test problem named find the number.

---
+++ Hackerrank.Day1.FindTheNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿class Result
+{
+    public static string findNumber(List<int> arr, int k)
+    {
+        if (arr == null || arr.Count == 0) return "NO";
+
+        for (int idx = 0; idx < arr.Count; idx++)
+        {
+            if (arr[idx] == k)
+            {
+                return "YES";
+            }
+        }
+
+        return "NO";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Result.findNumber(new List<int>() { 1, 2, 4 }, 5);
+    }
+}
